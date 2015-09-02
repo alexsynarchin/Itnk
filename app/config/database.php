1a55dd16d61996ae256b4a0a8d52489bc2045af0
alex <alexsynarchin@gmail.com>
2015-09-02 11:53:25 +0500

    change db pass

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'itnk',
 			'username'  => 'forge',
 			'password'  => 'BS0YnMcLu98T45WTYMAW',
 			'charset'   => 'utf8',
