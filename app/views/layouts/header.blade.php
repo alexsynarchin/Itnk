8c75d69abda21445b53237c40ca19447eca8d32a
alex <alexsynarchin@gmail.com>
2015-09-05 09:38:09 +0500

    Каркас аддмин панели

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -1,21 +1,10 @@
 <div class="container">
     <div class="row">
         <figure class="logo col-md-3">
-            <a href="#">
-                <img src="/images/logo.png"/>
-            </a>
+             <img src="/images/logo.png"/>
         </figure>
-        <div class="col-md-6">
-
-        </div>
-        @if (Auth::check())
-            <div class="auth_btns col-md-3">
-                <a class="btn btn-default" href="/logout">Выйти из системы</a>
-            </div>
-        @else
-            <div class="auth_btns  col-md-3">
+            <div class="auth_btns  col-md-3 col-md-offset-6">
                 <a class="btn btn-default" href="login">Войти в систему</a>
             </div>
-        @endif
     </div>
 </div>
--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -1,8 +1,1 @@
-@extends('layouts.app')
-<div class="container">
-    <div>
-        @if(Auth::check())
-            <p>Welcome to your profile page {{Auth::user()->first_name}} - {{Auth::user()->last_name}}</p>
-        @endif
-    </div>
-</div>
+@extends('layouts.admin.admin')
