9a12e14e1a63c0eef81705e540d056c3fd267750
alex <alexsynarchin@gmail.com>
2015-09-11 09:57:44 +0500

    commit

--- app/views/layouts/dashboard/js.blade.php
+++ app/views/layouts/dashboard/js.blade.php
@@ -5,8 +5,10 @@
     <!-- Bootstrap 3.3.5 -->
     <script src="/lte/bootstrap/js/bootstrap.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="/lte/dist/js/demo.js"></script>
-    <script src="/lte/dist/js/app.min.js"></script>
+<script src="/lte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="/lte/plugins/fastclick/fastclick.min.js"></script>
+<script src="/lte/dist/js/app.min.js"></script>
+<script src="/lte/dist/js/demo.js"></script>
 
     <!-- Optionally, you can add Slimscroll and FastClick plugins.
          Both of these plugins are recommended to enhance the
