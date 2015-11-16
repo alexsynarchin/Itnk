4df65923e4f09de769ac07a60acafdb3d6cc4dd0
alex <alexsynarchin@gmail.com>
2015-11-16 07:16:37 +0500

    Коммит

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -59,63 +59,6 @@
                     </ul>
                 </div>
             </div>
-    <div class="box ">
-        <div class="box-header with-border">
-            <h3 class="box-title"><i class="fa fa-file-text-o"></i> Основные средства</h3>
-        </div><!-- /.box-header -->
-        <div class="box-body ">
-            <table class="list table table-bordered table-striped table-hover">
-                <tr>
-                    <th>Наименование</th>
-                    <th>Инвентарный номер</th>
-                    <th>Код ОКОФ</th>
-                    <th>Балансовая стоимость</th>
-                    <th>Остаточная стоимость</th>
-                    <th>Дата ввода в эксплуатацию</th>
-                    <th>Действия</th>
-                </tr>
-                @foreach ($items as $item)
-                    <tr>
-                        <td>
-                            @if($item->name)
-                                {{{$item->name}}}
-                                @endif
-                            </td>
-                        <td>
-                            @if($item->number)
-                            {{{$item->number}}}
-                            @endif
-                        </td>
-                        <td>
-                            @if($item->okof)
-                            {{{$item->okof}}}
-                            @endif
-                        </td>
-                        <td>
-                            @if($item->carrying_amount)
-                            {{{$item->carrying_amount}}}
-                            @endif
-                        </td>
-                        <td>
-                            @if($item->variable->residual_value)
-                            {{{$item->variable->residual_value}}}
-                            @endif
-                        </td>
-                        <td></td>
-                        <td class="actions icons"><a href="{{action('ItemsController@getView',array($item->id))}}"><i class="fa fa-eye"></i></a><a href="{{action('ItemsController@getEdit',array($item->id))}}"><i class="fa fa-pencil-square-o"></i></a></td>
-                    </tr>
-                @endforeach
-            </table>
-        </div><!-- /.box-body -->
-        <div class="box-footer clearfix">
-            <ul class="pagination pagination-sm no-margin pull-right">
-                <li><a href="#">«</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">»</a></li>
-            </ul>
-        </div>
-    </div>
+  
 </section>
 @stop
